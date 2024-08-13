0f420c9524df9c2762605def7c860fe2152065d7
Maksim Elizarev <m.elizarev@imperial.ac.uk>
2024-08-13 14:27:58 +0100

    feat: :sparkles: capillary pressure under anisotropic permeability

--- src/CapPressure.m
+++ src/CapPressure.m
@@ -3,11 +3,12 @@ classdef CapPressure
         contact_angle (1,1) double = deg2rad(0)
         surface_tension (1,1) double = 20 * dyne() / (centi()*meter())
         leverett_j (1,1) TableFunction = TableFunction([]);
+        perm_weights (1,3) double {mustBeNonnegative} = [0.5,0.5,0]; % K = (Kx + Ky) / 2
         mult (1,1) double = nan;
     end
 
     methods
-        function obj = CapPressure(contact_angle,surface_tension,leverett_j)
+        function obj = CapPressure(contact_angle,surface_tension,leverett_j,perm_weights)
             if nargin==0
                 return;
             end
@@ -16,6 +17,8 @@ classdef CapPressure
             obj.surface_tension = surface_tension;
             obj.leverett_j = leverett_j;
             obj.mult = cos(contact_angle) * surface_tension;
+
+            obj.perm_weights = perm_weights./sum(perm_weights);
         end
 
         function pc = func(obj,sw,poro,perm)
@@ -25,6 +28,9 @@ classdef CapPressure
                 poro double {mustBeNonnegative}
                 perm double {mustBePositive}
             end
+
+            perm = obj.transform_perm(poro,perm);
+
             pc = obj.mult * obj.leverett_j.func(sw) * sqrt(poro./perm);
         end
 
@@ -35,6 +41,9 @@ classdef CapPressure
                 poro double {mustBePositive}
                 perm double {mustBeNonnegative}
             end
+
+            perm = obj.transform_perm(poro,perm);
+
             lj = pc ./ obj.mult .* sqrt(perm./poro);
 
             lj = min(lj,obj.leverett_j.data(1,2));
@@ -50,7 +59,23 @@ classdef CapPressure
                 poro double {mustBeNonnegative}
                 perm double {mustBePositive}
             end
+
+            perm = obj.transform_perm(poro,perm);
+
             dpc_dsw = obj.mult * obj.leverett_j.deriv(sw) * sqrt(poro./perm);
+        end
+
+        function perm = transform_perm(obj,poro,perm)
+            dims_poro = size(poro);
+            dims_perm = size(perm);
+            if (length(dims_poro) == 3) && (length(dims_perm) == 4) && (dims_perm(4) == 3)
+                perm = sum(perm .* reshape(obj.perm_weights, 1, 1, 1, 3), 4);
+                return;
+            end
+
+            if (length(dims_poro) == 2) && (length(dims_perm) == 2) && (dims_perm(2) == 3)
+                perm = sum(perm .* reshape(obj.perm_weights, 1, 3), 2);
+            end
         end
     end
 end
