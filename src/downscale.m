12019e8233f0efa34f6db58db0c813699e2e3802
Maksim Elizarev <m.elizarev@imperial.ac.uk>
2024-10-17 16:36:16 +0100

    refactor: :recycle: extract correlated field generator

--- src/downscale.m
+++ src/downscale.m
@@ -14,14 +14,14 @@ sub_perm_x = reshape(sub_poro_perm(2,:),gen_dims);
 
 %% generate fine-scale porosity
 
-sub_porosity = rsgen3D(dr,subscale_dims,downscale_params.corr_lens,@(N)sub_porosity);
+sub_porosity = gen_corr_field(dr,subscale_dims,downscale_params.corr_lens,@(N)sub_porosity);
 sub_porosity = normalize(sub_porosity,porosity);
 sub_porosity = max(sub_porosity,0);
 
 %% calculate fine-scale permeability
 
 sub_permeability_log = log(sub_perm_x);
-sub_permeability_log = rsgen3D(dr,subscale_dims,downscale_params.corr_lens,@(N)sub_permeability_log);
+sub_permeability_log = gen_corr_field(dr,subscale_dims,downscale_params.corr_lens,@(N)sub_permeability_log);
 perm_x = perm_coarse(1);
 sub_permeability_log = normalize(sub_permeability_log,log(perm_x));
 sub_perm_x = exp(sub_permeability_log);
@@ -32,40 +32,6 @@ sub_permeability = zeros([subscale_dims,3]);
 for i = 1:3
     sub_permeability(:,:,:,i) = sub_perm_x .* anisotropy(i);
 end
-end
-
-function [f] = rsgen3D(dr,dims,corr_lens,dist)
-arguments
-    dr        (1,3) double
-    dims      (1,3) uint32
-    corr_lens (1,3) double
-    dist      = @(N) randn(N)
-end
-x = linspace(-0.5,0.5,dims(1)).*dr(1)*(1 - 1/double(dims(1)));
-y = linspace(-0.5,0.5,dims(2)).*dr(2)*(1 - 1/double(dims(2)));
-z = linspace(-0.5,0.5,dims(3)).*dr(3)*(1 - 1/double(dims(3)));
-
-[X,Y,Z] = meshgrid(x,y,z);
-
-X = permute(X,[2 1 3]);
-Y = permute(Y,[2 1 3]);
-Z = permute(Z,[2 1 3]);
-
-D = dist(dims.*2-1);
-
-% Gaussian filter
-
-    function out = divide(a,b)
-        out = a./b;
-        out(a==0)=0;
-    end
-
-K = divide(X,corr_lens(1)).^2 + divide(Y,corr_lens(2)).^2 + divide(Z,corr_lens(3)).^2;
-K = exp(K.*(-2));
-K = K./sum(K(:));
-
-f = convn(D,K,'valid');
-
 end
 
 function [data] = normalize(data,new_norm)
