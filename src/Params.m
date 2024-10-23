5150cfa31147587c5dce39ddbbfd145c89b64f2b
Maksim Elizarev <m.elizarev@imperial.ac.uk>
2024-10-23 14:51:04 +0100

    feat: :sparkles: Hydrostatic correction for MIP
    Fixes #57

--- src/Params.m
+++ src/Params.m
@@ -3,17 +3,21 @@ classdef Params
         krw (1,1) TableFunction = TableFunction([]);
         krg (1,1) TableFunction = TableFunction([]);
         cap_pressure  (1,1) CapPressure
+        rho_gas (1,1) double
+        rho_water (1,1) double {mustBeFinite}
         sw_resid (1,1) double
     end
-    
+
     methods
-        function obj = Params(krw,krg,cap_pressure)
+        function obj = Params(krw,krg,cap_pressure, rho_gas,rho_water)
             obj.krw = krw;
             obj.krg = krg;
             obj.cap_pressure = cap_pressure;
 
             obj.sw_resid = obj.krw.data(1,1);
+
+            obj.rho_gas   = rho_gas;
+            obj.rho_water = rho_water;
         end
     end
 end
-
---
+++ src/units/std_gravity.m
@@ -0,0 +1,4 @@
+function g = std_gravity()
+    g = 9.80665 * meter() / sec()^2;
+end
+
