fca0ec491b6a6583e0f138034d1bcf7ed38ddde6
Maksim Elizarev <m.elizarev@imperial.ac.uk>
2024-11-22 11:13:09 +0000

    fix(export): :children_crossing: `ogs_export`: update input args to auto-create directories

--- ogs_export.m
+++ ogs_export.m
@@ -1,9 +1,13 @@
-function [] = ogs_export(G, strata_trapped, output_prefix)
+function [] = ogs_export(G, strata_trapped, output_folder)
 arguments
     G              (1,1) struct
     strata_trapped (1,1) struct
-    output_prefix  char   = 'model/gen/'
+    output_folder  char   = 'out'
 end
+
+mkdir(output_folder);
+
+output_prefix = append(output_folder,'/');
 
 write_mappings(output_prefix,G,strata_trapped.permeability ./ milli ./ darcy(),strata_trapped.idx);
 
