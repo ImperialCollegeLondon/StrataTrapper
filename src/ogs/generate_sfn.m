37fb09f1faa6285e425bd9eced1cff2ed23cdbdb
Maksim Elizarev <44438314+djmaxus@users.noreply.github.com>
2025-02-06 17:21:24 +0000

    Update generate_sfn.m

--- src/ogs/generate_sfn.m
+++ src/ogs/generate_sfn.m
@@ -52,19 +52,3 @@ for cell_index = 1:length(idx)
         'END');
 end
 end
-
-function write_sfn_table(file_id,table_name,sat,kr,pc)
-arguments
-    file_id
-    table_name string
-    sat (1,:) double
-    kr (1,:) double
-    pc (1,:) double
-end
-
-data = [sat;kr;pc];
-
-fprintf(file_id,'%s\n',table_name);
-fprintf(file_id,'%e\t%e\t%e\n',data);
-fprintf(file_id,'%s\n','/');
-end
