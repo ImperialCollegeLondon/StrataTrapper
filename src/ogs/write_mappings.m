b2e7ee643c1a20bcd4e6b77b05bde0c5b855ee3d
Maksim Elizarev <44438314+djmaxus@users.noreply.github.com>
2025-02-11 09:00:42 +0000

    Update write_mappings.m

--- src/ogs/write_mappings.m
+++ src/ogs/write_mappings.m
@@ -6,7 +6,7 @@ keywords = ["KRNUMX","KRNUMY","KRNUMZ","SATNUM"];
 num_cells = length(idx);
 num_offset = [0,num_cells,num_cells*2,0];
 
-parfor keyword_num = 1:length(keywords)
+for keyword_num = 1:length(keywords)
     keyword = keywords(keyword_num);
     file_name = join([prefix,keyword,".inc"],'');
     write_curve_nums(file_name,keyword,curve_mapping,1,num_offset(keyword_num));
