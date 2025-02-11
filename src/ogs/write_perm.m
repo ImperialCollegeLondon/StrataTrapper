cfd561a6443c25af25985b5b064b4f1fd0c16dc8
Maksim Elizarev <44438314+djmaxus@users.noreply.github.com>
2025-02-11 09:00:40 +0000

    Update write_perm.m

--- src/ogs/write_perm.m
+++ src/ogs/write_perm.m
@@ -3,7 +3,7 @@ perm = perm / milli / darcy;
 perm_mapping = zeros(G.cells.num,3);
 perm_mapping(G.cells.indexMap(idx),:) = perm;
 keywords = ["PERMX","PERMY","PERMZ"];
-parfor keyword_num = 1:length(keywords)
+for keyword_num = 1:length(keywords)
     keyword = keywords(keyword_num);
     file_name = join([prefix,keyword,".inc"],'');
     write_curve_nums(file_name,keyword,perm_mapping(:,keyword_num),0,0);
