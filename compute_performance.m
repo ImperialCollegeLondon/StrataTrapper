4a4bfc1dc1c1a23e073d18f80cb3f26255140d78
Maksim Elizarev <44438314+djmaxus@users.noreply.github.com>
2025-03-07 18:51:19 +0000

    Update compute_performance.m

--- compute_performance.m
+++ compute_performance.m
@@ -1,5 +1,12 @@
-function [perf] = compute_performance(perf)
-perf.multi_MHz = double(perf.num_sat * sum(perf.num_sub.^3)) / double(perf.elapsed) * 10^(-6);
-perf.single_MHz = perf.multi_MHz / double(perf.num_workers);
-end
+function [perf] = compute_performance(perf,k)
+perf.k = k;
 
+perf = structfun(@(field)double(field),perf,'UniformOutput',false);
+
+numerator = (2*perf.num_sat+1) * perf.num_sub.^k;
+perf.single = numerator ./ perf.elapsed;
+
+perf.single_avg = sum(numerator) / sum(perf.elapsed);
+
+perf.total = perf.single_avg * perf.num_workers;
+end
