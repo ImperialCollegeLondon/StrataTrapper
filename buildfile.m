11b37d0f7a7a3e99150702be6363c074e000b2eb
Maksim Elizarev <44438314+djmaxus@users.noreply.github.com>
2025-04-11 16:28:29 +0100

    chore(matlab-repo-init): ⬆️ adopt v13.2

--- buildfile.m
+++ buildfile.m
@@ -1,15 +1,27 @@
 function plan = buildfile()
+
 plan = buildplan(localfunctions);
+plan.DefaultTasks = ["check", "test"];
 
 if isMATLABReleaseOlderThan("R2023b")
     plan("test") = matlab.buildtool.Task( ...
     Description="Run tests", ...
     Actions=@(~) assert(runtests().Failed == 0));
-    plan.DefaultTasks = "test";
+
+    plan("check") = matlab.buildtool.Task(...
+    Description="Identify code issues", ...
+    Actions=@(~) assert(code_issues()) );
+    
     return;
 end
 
 plan("check") = matlab.buildtool.tasks.CodeIssuesTask(WarningThreshold=0);
 plan("test") = matlab.buildtool.tasks.TestTask;
-plan.DefaultTasks = ["check","test"];
+
+end
+
+function issue_free = code_issues()
+    issues = codeIssues();
+    disp(issues.Issues);
+    issue_free = isempty(issues.Issues);
 end
