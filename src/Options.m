4d5ce1d4da92141f801a3b2719033961313e8e76
djmaxus <44438314+djmaxus@users.noreply.github.com>
2025-04-25 11:56:12 +0100

    feat(options): ✨ save sub-saturations from MIP step
    Adds support for saving MIP step sub-saturations
    
    Introduces an option to save sub-saturations during the MIP step.
    Updates the `Options` class to include a `save_mip_step` method and a corresponding flag.
    Modifies related functions to handle and store sub-saturation data when the option is enabled.
    Enhances debugging and analysis capabilities by retaining intermediate MIP results.
    Enables a posteriori downscaling capabilities.

--- src/Options.m
+++ src/Options.m
@@ -3,5 +3,12 @@ classdef Options
         sat_num_points (1,1) uint32 {mustBePositive} = 40
         sat_tol (1,1) double {mustBeNonnegative} = 1e-4
         hydrostatic_correction (1,1) logical = false
+        m_save_mip_step (1,1) {mustBeOfClass(m_save_mip_step,'logical')} = false;
+    end
+
+    methods % builders
+        function self = save_mip_step(self,flag)
+            self.m_save_mip_step = flag;
+        end
     end
 end
