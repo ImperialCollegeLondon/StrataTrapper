35298d4c568a3f15e83b9e6fdb418fec47520db3
djmaxus <44438314+djmaxus@users.noreply.github.com>
2025-05-19 16:24:25 +0100

    fix(export): `write_curve_num` -> `write_keyword`
    replace `write_curve_nums` with `write_keyword`
    
    Replaces the deprecated `write_curve_nums` function with the newly implemented
    `write_keyword` function to improve code clarity and maintainability.
    
    The new implementation uses `fopen` and `fprintf` for writing data, addressing
    previous inefficiencies in handling file output. Updates all references to the
    old function for compatibility.
    
    src/ogs/write_keyword.m #	modified:   src/ogs/write_mappings.m #	modified:
    src/ogs/write_perm.m #

--- src/ogs/write_perm.m
+++ src/ogs/write_perm.m
@@ -6,7 +6,7 @@ keywords = ["PERMX","PERMY","PERMZ"];
 for keyword_num = 1:length(keywords)
     keyword = keywords(keyword_num);
     file_name = join([prefix,keyword,".inc"],'');
-    write_curve_nums(file_name,keyword,perm_mapping(:,keyword_num),0,0);
+    write_keyword(file_name,keyword,perm_mapping(:,keyword_num),0,0);
 end
 end
 
