88ce122726d1bbb48bc49168f60f4d3b8f40b168
djmaxus <44438314+djmaxus@users.noreply.github.com>
2025-06-09 17:27:48 +0100

    perf: ⚡️ `calc_percolation` is now vectorized

--- src/calc_percolation.m
+++ src/calc_percolation.m
@@ -3,7 +3,10 @@ function [invasion,num_iter] = calc_percolation(p_boundary,p_entry, ...
 
 h_ref = Lz*0.5;
 
-Nz = size(p_entry,3);
+N = size(p_entry);
+Nx = N(1);
+Ny = N(2);
+Nz = N(3);
 h(1,1,1:Nz) = linspace(Lz/Nz/2,Lz - Lz/Nz/2,Nz);
 
 hydrostatic_correction = 0;
@@ -11,82 +14,41 @@ if ~ismissing(rho_gas)
     hydrostatic_correction = include_gravity * std_gravity() * (rho_water - rho_gas) * (h - h_ref);
 end
 
-invasion = false(size(p_entry));
-invadable = (p_entry + hydrostatic_correction) < p_boundary;
-idx_invadable = find(invadable)';
-[I,J,K] = ind2sub(size(invadable),idx_invadable);
+is_invadable = (p_entry + hydrostatic_correction) < p_boundary;
+is_boundary = false(size(p_entry));
+is_boundary(  1,  :,  :) = true;
+is_boundary(end,  :,  :) = true;
+is_boundary(  :,  1,  :) = true;
+is_boundary(  :,end,  :) = true;
+is_boundary(  :,  :,  1) = true;
+is_boundary(  :,  :,end) = true;
+invasion = is_invadable & is_boundary;
+
 is_invading = true;
 num_iter = 0;
 while is_invading
-    [invasion, is_invading] = calc_percolation_iter(invasion,[I;J;K]);
-    not_checked = find(invadable & ~invasion)';
-    [I,J,K] = ind2sub(size(invadable),not_checked);
     num_iter = num_iter+1;
-end
-end
-
-function [invasion,has_changed] = calc_percolation_iter(invasion,Idx)
-[Nx,Ny,Nz] = size(invasion);
-has_changed = false;
-for idx = Idx
-    i = idx(1);
-    j = idx(2);
-    k = idx(3);
-
-    is_boundary = i==1 || i==Nx || j==1 || j==Ny || k==1 || k==Nz;
-
-    is_connected = is_boundary;
-
-    if ~is_connected
-        is_connected = find_invaded_nearby(invasion, i,j,k, Nx,Ny,Nz);
-    end
-
-    if ~is_connected
-        continue;
-    end
-
-    invasion(i,j,k) = true;
-    has_changed = true;
-end
+    has_invaded_nearby = logical(convn(invasion,near_kernel(),"same"));
+    new_invaded = is_invadable & has_invaded_nearby;
+    is_invading = any(xor(invasion(:),new_invaded(:)));
+    invasion = invasion | new_invaded;
 end
 
-function is_next_to_invaded = find_invaded_nearby(invasion, i,j,k, Nx,Ny,Nz)
-is_next_to_invaded = false;
-delta_around = [-1,1];
-
-for di=delta_around
-    ii = i + di;
-    if ii < 1 || ii > Nx
-        continue;
-    end
-
-    is_next_to_invaded = invasion(ii,j,k);
-    if is_next_to_invaded
-        return;
-    end
 end
 
-for dj=delta_around
-    jj = j + dj;
-    if jj < 1 || jj > Ny
-        continue;
-    end
-
-    is_next_to_invaded = invasion(i,jj,k);
-    if is_next_to_invaded
-        return;
-    end
+function out = near_kernel()
+persistent k
+if isempty(k)
+    k = cat(3, ...
+        [0,0,0; ...
+        0,1,0; ...
+        0,0,0], ...
+        [0,1,0; ...
+        1,0,1; ...
+        0,1,0], ...
+        [0,0,0; ...
+        0,1,0; ...
+        0,0,0]);
 end
-
-for dk=delta_around
-    kk = k + dk;
-    if kk < 1 || kk > Nz
-        continue;
-    end
-
-    is_next_to_invaded = invasion(i,j,kk);
-    if is_next_to_invaded
-        return;
-    end
-end
+out = k;
 end
