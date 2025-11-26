054452b81aaa56f7e80e06c35d255b9b1eba1277
djmaxus <44438314+djmaxus@users.noreply.github.com>
2025-11-26 08:55:19 +0000

    WIP export with default rock props

--- src/export/opm_export.m
+++ src/export/opm_export.m
@@ -1,11 +1,13 @@
-function [] = opm_export(strata_trapped, output_folder)
+function [] = opm_export(strata_trapped, args)
 arguments
     strata_trapped (1,1) struct
-    output_folder  char   = 'opm'
+    args.output_folder  char   = 'opm'
+    args.default_poro (:,1) double = []
+    args.default_perm (:,3) double = []
 end
 
-mkdir(output_folder);
-output_prefix = append(output_folder,'/');
+mkdir(args.output_folder);
+output_prefix = append(args.output_folder,'/');
 
 tab_dims = 1 + numel(strata_trapped.idx)*3;
 write_keyword([output_prefix,'TABDIMS.inc'],'TABDIMS',tab_dims,0,0);
@@ -14,11 +16,15 @@ grid = strata_trapped.grid;
 
 % export porosity
 porosity = zeros(grid.cells.num,1);
+if ~isempty(args.default_poro)
+    porosity(:) = args.default_poro;
+end
 porosity(grid.cells.indexMap(strata_trapped.idx)) = strata_trapped.porosity;
 write_keyword([output_prefix,'PORO.inc'],'PORO',porosity,0,0);
 
 % export permeability
-write_perm(output_prefix,strata_trapped.grid,strata_trapped.permeability,strata_trapped.idx);
+write_perm(output_prefix,strata_trapped.grid,strata_trapped.permeability,strata_trapped.idx,...
+    args.default_perm);
 
 % Write JFUNC include file
 mult = strata_trapped.params.cap_pressure.mult / (dyne / centi / meter);
@@ -70,11 +76,63 @@ fclose(regions_fid);
 % Write single SGWFN file: 1 + NX*NY*NZ*3 tables
 write_sgwfn(strata_trapped,output_prefix);
 
+% FIXME: add ENDSCALE and umbrella RUNSPEC include file
+% TODO add FIPNUM region for MIP-upscaled cells
+end
+
+function write_sgwfn(strata_trapped,prefix)
+arguments
+    strata_trapped
+    prefix char
+end
+
+original_sgwfn = strata_trapped.params.export_opm(strata_trapped.saturation);
+
+leverett_j = strata_trapped.params.cap_pressure.inv_lj(...
+    strata_trapped.capillary_pressure,...
+    strata_trapped.porosity,...
+    strata_trapped.permeability);
+
+sgwfn_fid = fopen([prefix,'SGWFN.inc'],'wb','native','UTF-8');
+fprintf(sgwfn_fid,'%s\n',"SGWFN");
+% 1. Write original table
+fprintf(sgwfn_fid,'%s/ -- 1: original fine-scale curves\n',original_sgwfn);
+% 2. Write upscaled tables
+for direction=1:3
+    write_tables_for_direction(sgwfn_fid, strata_trapped.idx,...
+        strata_trapped.rel_perm_wat, strata_trapped.rel_perm_gas,...
+        strata_trapped.saturation,leverett_j,...
+        direction);
+end
+fclose(sgwfn_fid);
+
+end
+
+function write_tables_for_direction(file_id,idx, krw, krg,saturations,leverett_j,direction)
+dir_label = ['X','Y','Z'];
+for cell_index = 1:length(idx)
+    sw = saturations;
+    sg = 1 - sw;
+    krg_cell = squeeze(krg(cell_index,direction,:))';
+    krw_cell  = squeeze(krw(cell_index,direction,:))';
+    jfunc = leverett_j(cell_index,:);
+    data = [sg;krg_cell;krw_cell;jfunc];
+    data = data(:,end:-1:1);
+
+    fprintf(file_id,'%e\t%e\t%e\t%e\n',data);
+    fprintf(file_id,'/ -- KRNUM%s cell %u\n',dir_label(direction),idx(cell_index));
+end
+end
+
+% Sketch
 %{
 RUNSPEC
 
 TABDIMS
 1 -- 1 + NX*NY*NZ*3
+/
+
+ENDSCALE
 /
 
 GRID
@@ -130,49 +188,3 @@ KRNUMZ
 115200*3
 /
 %}
-
-end
-
-function write_sgwfn(strata_trapped,prefix)
-arguments
-    strata_trapped
-    prefix char
-end
-
-original_sgwfn = strata_trapped.params.export_opm(strata_trapped.saturation);
-
-leverett_j = strata_trapped.params.cap_pressure.inv_lj(...
-    strata_trapped.capillary_pressure,...
-    strata_trapped.porosity,...
-    strata_trapped.permeability);
-
-sgwfn_fid = fopen([prefix,'SGWFN.inc'],'wb','native','UTF-8');
-fprintf(sgwfn_fid,'%s\n',"SGWFN");
-% 1. Write original table
-fprintf(sgwfn_fid,'%s/ -- 1: original fine-scale curves\n',original_sgwfn);
-% 2. Write upscaled tables
-for direction=1:3
-    write_tables_for_direction(sgwfn_fid, strata_trapped.idx,...
-        strata_trapped.rel_perm_wat, strata_trapped.rel_perm_gas,...
-        strata_trapped.saturation,leverett_j,...
-        direction);
-end
-fclose(sgwfn_fid);
-
-end
-
-function write_tables_for_direction(file_id,idx, krw, krg,saturations,leverett_j,direction)
-dir_label = ['X','Y','Z'];
-for cell_index = 1:length(idx)
-    sw = saturations;
-    sg = 1 - sw;
-    krg_cell = squeeze(krg(cell_index,direction,:))';
-    krw_cell  = squeeze(krw(cell_index,direction,:))';
-    jfunc = leverett_j(cell_index,:);
-    data = [sg;krg_cell;krw_cell;jfunc];
-    data = data(:,end:-1:1);
-
-    fprintf(file_id,'%e\t%e\t%e\t%e\n',data);
-    fprintf(file_id,'/ -- KRNUM%s cell %u\n',dir_label(direction),idx(cell_index));
-end
-end
