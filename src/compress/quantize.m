47dd804705833056a2c3b23fce8d60fb8b00b62d
Max Elizarev <44438314+djmaxus@users.noreply.github.com>
2026-03-01 22:03:49 +0000

    feat: ✨ implement quantization function for compression

---
+++ src/compress/quantize.m
@@ -0,0 +1,12 @@
+function [compressed] = quantize(strata_trapped,args)
+arguments (Input)
+    strata_trapped
+    args
+end
+arguments (Output)
+    compressed
+end
+
+compressed = struct([]);
+
+end
