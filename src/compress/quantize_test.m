1c5fc36748eb6d6463f19dbcff8e0a25691aa64c
djmaxus <44438314+djmaxus@users.noreply.github.com>
2026-03-06 18:30:45 +0000

    feat: implement organize function to structure tables and integrate into strata_trapper

--- src/compress/quantize_test.m
+++ src/compress/quantize_test.m
@@ -19,30 +19,4 @@ err = compare_tables(quantized_trivial.tables,quantized.tables,false);
 [err.delta_mse]
 %%
 
-organised = organise(strata_trapped);
-
-function output = organise(strata_trapped)
-% TODO: support multiple param_ids
-
-tables = repmat(struct('leverett_j',[],'krw',[],'krg',[],'mapping',[]),...
-    numel(strata_trapped.params),3);
-
-for param_id=1:numel(strata_trapped.params)
-
-    mask = strata_trapped.param_ids == param_id;
-
-    leverett_j= strata_trapped.params(param_id).cap_pressure.inv_lj(...
-        strata_trapped.capillary_pressure(mask,:),...
-        strata_trapped.porosity(mask,:),...
-        strata_trapped.permeability(mask,:));
-
-    for dir=1:3
-        tables(param_id,dir).leverett_j = leverett_j;
-        tables(param_id,dir).krw = squeeze(strata_trapped.rel_perm_wat(mask,dir,:));
-        tables(param_id,dir).krg = squeeze(strata_trapped.rel_perm_gas(mask,dir,:));
-        tables(param_id,dir).mapping = 1:numel(strata_trapped.idx(mask));
-    end
-end
-output = rmfield(strata_trapped,{'capillary_pressure','rel_perm_wat','rel_perm_gas'});
-output.tables = tables;
-end
+plot_result(organised,1,"visible",'off');
