0081407a8da0af3a5b352933db757b49fd8de80c
djmaxus <44438314+djmaxus@users.noreply.github.com>
2026-03-06 18:15:47 +0000

    fix: adjust compare_tables function to handle dynamic table sizes

--- src/compress/compare_tables.m
+++ src/compress/compare_tables.m
@@ -1,16 +1,19 @@
 function err = compare_tables(tables_1,tables_2,fit_total_mobility)
 
-err = repmat(struct('delta',[],'delta_mse',Inf),3,1);
+err = repmat(struct('delta',[],'delta_mse',Inf),size(tables_1));
 
-for dir=1:3
+for param_id = 1:size(tables_1,1)
+    for dir=1:3
 
-    feat_dir_1 = to_features(tables_1(dir),fit_total_mobility);
-    feat_dir_full_1 = feat_dir_1(:,tables_1(dir).mapping);
+        feat_dir_1 = to_features(tables_1(param_id,dir),fit_total_mobility);
+        feat_dir_full_1 = feat_dir_1(:,tables_1(param_id,dir).mapping);
 
-    feat_dir_2 = to_features(tables_2(dir),fit_total_mobility);
-    feat_dir_full_2 = feat_dir_2(:,tables_2(dir).mapping);
+        feat_dir_2 = to_features(tables_2(param_id,dir),fit_total_mobility);
+        feat_dir_full_2 = feat_dir_2(:,tables_2(param_id,dir).mapping);
 
-    err(dir).delta = feat_dir_full_2 - feat_dir_full_1;
-    err(dir).delta_mse = norm(err(dir).delta,"fro")/sqrt(numel(err(dir).delta));
+        err(param_id,dir).delta = feat_dir_full_2 - feat_dir_full_1;
+        err(param_id,dir).delta_mse = norm(err(param_id,dir).delta,"fro")/...
+            sqrt(numel(err(param_id,dir).delta));
+    end
 end
 end
--- src/compress/quantize_test.m
+++ src/compress/quantize_test.m
@@ -6,7 +6,7 @@ quantized_trivial = quantize(organised,QuantizeOptions());
 options = QuantizeOptions();
 options.fit_total_mobility = false;
 options.dim_reduction = "PCA";
-options.num_quants = 2;
+options.num_quants = 1;
 options.duplicate_threshold = 0.0;
 options.num_pc = 3;
 
