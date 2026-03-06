65fa68b8cb1cabf96398d5449bd47267d42cfdc6
djmaxus <44438314+djmaxus@users.noreply.github.com>
2026-03-06 15:41:19 +0000

    clustering without dedup and dim reduction

--- src/compress/QuantizeOptions.m
+++ src/compress/QuantizeOptions.m
@@ -5,7 +5,8 @@ classdef QuantizeOptions
         dim_reduction (1,1) DimReduction = DimReduction.None
         num_quants (:,1) uint32 {mustBeScalarOrEmpty, mustBePositive} = []
         % NOTE: name-value arguments as per official kmeans documentation
-        kmeans = {'OnlinePhase','off','MaxIter',1000,'Display','iter','Distance','sqeuclidean'} 
+        kmeans = {'OnlinePhase','off','MaxIter',1000,'Display','iter','Distance','sqeuclidean',...
+            'EmptyAction','drop','Start','plus'} 
     end
 
     methods
--- src/compress/quantize_test.m
+++ src/compress/quantize_test.m
@@ -1,11 +1,9 @@
-clc;
-
 quantized_trivial = quantize(strata_trapped,QuantizeOptions());
 
 options = QuantizeOptions();
 options.use_total_mobility = false;
 options.dim_reduction = "None";
-options.num_quants = 4000;
+options.num_quants = 1000;
 
 quantized = quantize(strata_trapped,options);
 
