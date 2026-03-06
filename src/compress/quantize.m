46b9a20839229b93d8941684f44746f986e94d91
djmaxus <44438314+djmaxus@users.noreply.github.com>
2026-03-06 15:58:57 +0000

    feat: ✨ update duplicate_threshold property to allow empty initialization and improve deduplication logic

--- src/compress/quantize.m
+++ src/compress/quantize.m
@@ -12,7 +12,7 @@ leverett_j = strata_trapped.params.cap_pressure.inv_lj(strata_trapped.capillary_
 
 tables = repmat(struct('leverett_j',[],'krw',[],'krg',[],'mapping',[]),3,1);
 for dir=1:3
-    tables(dir).leverett_j = leverett_j; 
+    tables(dir).leverett_j = leverett_j;
     tables(dir).krw = squeeze(strata_trapped.rel_perm_wat(:,dir,:));
     tables(dir).krg = squeeze(strata_trapped.rel_perm_gas(:,dir,:));
     tables(dir).mapping = 1:numel(strata_trapped.idx);
@@ -43,7 +43,6 @@ end
 features = to_features(tables_dir,options.use_total_mobility);
 
 mapping_dedup = deduplicate(features,options.duplicate_threshold);
-mapping_dedup(2) = 1; % FIXME: remove this test code
 
 tables_dedup = from_quants(tables_dir,mapping_dedup);
 
@@ -63,10 +62,26 @@ quantized = from_features(tables_dedup.mapping, features_quant, mapping_quant, .
 end
 
 function mapping_dedup = deduplicate(features,duplicate_threshold)
-    if ~isempty(duplicate_threshold)
-        mapping_dedup = 1:size(features,2);
-        return;
+if isempty(duplicate_threshold)
+    mapping_dedup = 1:size(features,2);
+    return;
+end
+
+num_samples = size(features,2);
+
+mapping_dedup = zeros(1,num_samples);
+
+checksums = sum(abs(features),1);
+
+for ref_sample_num=1:num_samples
+    % exit early if all mappings are ready
+    if all(mapping_dedup)
+        break;
     end
+    % find all close checksums to ref_sample_num
+    is_sample_close = abs(checksums - checksums(ref_sample_num)) <= duplicate_threshold;
+    is_sample_close = is_sample_close*ref_sample_num;
+end
 end
 
 function [features_quant,mapping_quant] = quantize_impl(features,options)
@@ -75,7 +90,7 @@ function [features_quant,mapping_quant] = quantize_impl(features,options)
 encoded = [];
 decoder = [];
 switch options.dim_reduction
-    case DimReduction.None    
+    case DimReduction.None
         % Trivial transform
         encoded = features;
         decoder = @(x) x;
@@ -85,7 +100,7 @@ switch options.dim_reduction
     case DimReduction.Correlations
         % feature vector is a parametric fit
         [encoded, decoder] = reduce_corr(quantized);
-end    
+end
 
 [mapping_quant,quants,~,~] = kmeans(encoded',options.num_quants,options.kmeans{:});
 
@@ -94,41 +109,41 @@ features_quant = decoder(quants');
 end
 
 function tables = from_features(mapping_prev, features, mapping_new, use_total_mobility)
-    tables = struct();
+tables = struct();
 
-    features = features';
+features = features';
 
-    table_dim = size(features,2)/3;
+table_dim = size(features,2)/3;
 
-    tables.leverett_j = 10.^features(:,1:table_dim);
-    tables.krg = features(:, (2*table_dim+1):end);
+tables.leverett_j = 10.^features(:,1:table_dim);
+tables.krg = features(:, (2*table_dim+1):end);
 
-    krw_or_tm = features(:, (table_dim+1):(2*table_dim));
+krw_or_tm = features(:, (table_dim+1):(2*table_dim));
 
-    tables.krw = max(krw_or_tm .* (1+use_total_mobility) - tables.krg.*use_total_mobility,0);
+tables.krw = max(krw_or_tm .* (1+use_total_mobility) - tables.krg.*use_total_mobility,0);
 
-    tables.mapping = merge_maps(mapping_prev,mapping_new);
+tables.mapping = merge_maps(mapping_prev,mapping_new);
 end
 
 function tables_quant = from_quants(tables,mapping)
-    tables_quant = tables;
-    [quant_idx, ~, ic] = unique(mapping);
-    tables_quant.leverett_j = tables_quant.leverett_j(quant_idx,:);
-    tables_quant.krw = tables_quant.krw(quant_idx,:);
-    tables_quant.krg = tables_quant.krg(quant_idx,:);
-    tables_quant.mapping = ic';
+tables_quant = tables;
+[quant_idx, ~, ic] = unique(mapping);
+tables_quant.leverett_j = tables_quant.leverett_j(quant_idx,:);
+tables_quant.krw = tables_quant.krw(quant_idx,:);
+tables_quant.krg = tables_quant.krg(quant_idx,:);
+tables_quant.mapping = ic';
 end
 
 function [encoded, decoder] = reduce_pca(quantized)
-    error("not implemented");
+error("not implemented");
 end
 
 function [encoded, decoder] = reduce_corr(quantized)
-    error("not implemented");
+error("not implemented");
 end
 
 function mapping = merge_maps(map_1, map_2)
-    [~, ~, inv_1] = unique(map_1);
-    [~, ~, inv_2] = unique(map_2);
-    mapping = inv_2(inv_1);
+[~, ~, inv_1] = unique(map_1);
+[~, ~, inv_2] = unique(map_2);
+mapping = inv_2(inv_1);
 end
